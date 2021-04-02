3ae3c18a561df25f8a40d05ae39d70076118f33f
sedx876 <swatkins876@gmail.com>
2021-04-02 05:54:02 -0400

    Initial commit from machine

---
+++ src/Email.java
@@ -0,0 +1,26 @@
+
+
+public class Email {
+  private String firstName;
+  private String lastName;
+  private String password;
+  private String department;
+  private int mailboxCapacity;
+  private String alternateEmail;
+
+  //Constructor
+  public Email(String firstName, String lastName){
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+
+  //Ask for department
+
+  //Generate random password
+
+  //Set the mailbox capacity
+
+  //Set the alternate email
+
+  //Change password
+}
